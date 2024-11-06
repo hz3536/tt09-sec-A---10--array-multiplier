d7bf637c1b9ed3e4f6a8d8c041c6e859ca1310cf
hz3536 <hz3536@nyu.edu>
2024-11-06 18:42:05 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_arry_mult_structural (
+module tt_um_sec_A_10_arry_mult_structural (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
